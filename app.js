4d5a254fbf954f437273b88da27cf37a828a6355
neoseurae12 <87654809+neoseurae12@users.noreply.github.com>
2022-09-27 16:03:20 +0900

    2.3 const & let

---
+++ app.js
@@ -0,0 +1,13 @@
+const a = 5;
+const b = 2;
+
+const myName = "rowan";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello " + myName);
+
+myName = "Wonhui Roh";
+
+console.log("my new name is " + myName);
