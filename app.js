aaa5ed6457e20a20cb2d36f03d93c9dd1e717270
neoseurae12 <87654809+neoseurae12@users.noreply.github.com>
2022-09-27 16:47:42 +0900

    2.5 Arrays

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-const amIHealthy = true;    // Boolean
-console.log(amIHealthy);
+const daysofWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+const nonsense = [1, 2, "hello", false, null, undefined, true, "rowan"];
 
-const brain = null; // null
-console.log(brain);
+console.log(daysofWeek, nonsense);
 
-let something;  // undefined
-console.log(something);
+// Get Item from Array
+console.log(daysofWeek[4]);
+
+// Add one more day to the array
+daysofWeek.push('sun');
+console.log(daysofWeek);
+
+// const daysofWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+// daysofWeek.push('sun');
+// => const는 '상수'라 불변하다고 했는데 이게 어떻게 가능?
+// : const는 '선언한 주소(박스)'를 바꿀수 없다는 말임.
+// 따라서 박스 안의 내용물들을 추가하거나 삭제는 가능함.
